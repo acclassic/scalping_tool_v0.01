089372bb0a7d639f48a54553532695ea0ce4b4c5
AcClassic <cantillo.trd@gmail.com>
2022-09-21 19:21:52 +0200

    Cleaned ws_handler.

--- apintrf/binance/ws_handler.go
+++ apintrf/binance/ws_handler.go
@@ -2,6 +2,7 @@ package binance
 
 import (
 	"context"
+	"strings"
 	"sync"
 )
 
@@ -29,48 +30,24 @@ func (mp *marketPrice) get_price(wg *sync.WaitGroup) float64 {
 	return mp.price
 }
 
-//TODO replace markets with global var
 func resp_hander(ctx context.Context, resp *WsStream) {
+	//TODO check of errors are handled and how a disconect is handled.
 	//If resp empty ignore, means sendet req was sucessfull
-	//if len(resp.Data.Asks) <= 0 && len(resp.Data.Bids) <= 0 {
-	//	return
-	//}
+	if len(resp.Data.Asks) <= 0 && len(resp.Data.Bids) <= 0 {
+		return
+	}
 
 	////TODO change trim to split after @ and take first slice
-	//markt := strings.TrimSuffix(resp.Stream, "@depth5@100ms")
-	//switch strings.ToUpper(markt) {
-	//case trdStrategy.BuyMarket:
-	//	price, _ := resp.Data.Asks[0][0].Float64()
-	//	//price, _ := resp.Data.Asks[1][0].Float64()
-	//	buyMarketP.update_price(price)
-	//case trdStrategy.SellMarket:
-	//	price, _ := resp.Data.Bids[0][0].Float64()
-	//	//price, _ := resp.Data.Bids[1][0].Float64()
-	//	sellMarketP.update_price(price)
-	//case trdStrategy.ConvMarket:
-	//	price, _ := resp.Data.Asks[0][0].Float64()
-	//	//price, _ := resp.Data.Asks[2][0].Float64()
-	//	convMarketP.update_price(price)
-	//}
-	//TODO check with sleeper and witohout goroutine
-	//for {
-	//	select {
-	//	case <-ctx.Done():
-	//		fmt.Println("return")
-	//		return
-	//	default:
-	//		fmt.Println(time.UnixMilli(resp.Data.S))
-	//	}
-	//}
-	//DEL
-	//params := queryParams{
-	//	"symbol": "EURUSDT",
-	//}
-	//reqParams := httpReq{
-	//	method:  "GET",
-	//	url:     "/api/v3/avgPrice",
-	//	qParams: params,
-	//}
-	//http_req_handler(ctx, reqParams)
-	//DEL
+	markt := strings.TrimSuffix(resp.Stream, "@depth5@100ms")
+	switch strings.ToUpper(markt) {
+	case trdStrategy.BuyMarket:
+		price, _ := resp.Data.Asks[0][0].Float64()
+		buyMarketP.update_price(price)
+	case trdStrategy.SellMarket:
+		price, _ := resp.Data.Bids[0][0].Float64()
+		sellMarketP.update_price(price)
+	case trdStrategy.ConvMarket:
+		price, _ := resp.Data.Asks[0][0].Float64()
+		convMarketP.update_price(price)
+	}
 }
